8e90a89f32342c92abb2910af9573b17624a5d96
Treasure185 <treasureginindza15@gmail.com>
2024-11-18 15:48:39 +0200

    Add files via upload

---
+++ world clock HTML.cpp
@@ -0,0 +1,31 @@
+world clock HTML
+<h1>world clock pro HTML</h1>
+<select>
+<option value="">select a city</ption>
+<option value="city">Los angels</option>
+<option va;e="city">London</option>
+<option value="city">New York</opion>
+<option value="city">Auckland<?option>
+
+</select>
+<div class="city"
+<h2> Los angels</h2>
+<h3> London</h3>
+<h4>New York</h4>
+<h5>Auckland</h5>
+<div class="date">November 18nth 2024</div>
+<div class="time">15:38 PM</div>
+
+
+
+<a href=https://codesandbox.io/p/sandbox/js-time-zones-challenge-forked-rdz62p
+
+
+<footer>coded by Gugu Ginindza❤and is 
+
+<a href="#"target"-blank">open sourced on github</a>
+
+</footer>
+
+<link rel="stylesheet"ref=index js/style css/>
+<script src="index.js"></script>
